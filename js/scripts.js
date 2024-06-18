ad0488fb3701443f119ccbd11384f3c501ab9223
knightrid3r8998 <knightrid3r92@gmail.com>
2024-06-18 22:20:48 +0900

    Add existing file

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+/*!
+* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel)
+
+// fancybox scripts
+$("[data-fancybox='gallery']").fancybox({
+    buttons: [
+        "slideShow",
+        "zoom",
+        "fullScreen",
+        "close",,
+    ]
+    loop : true,
+    protected : true,
+})
+
